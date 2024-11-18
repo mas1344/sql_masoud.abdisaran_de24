43ace6b30043d123601a5a5714d8d08df2ade28e
mas1344 <masoudabdi@hotmail.com>
2024-11-18 13:53:02 +0100

    ingesting salaries

---
+++ live_demoes/05_filtering/sql/where_clause.sql
@@ -0,0 +1,54 @@
+SELECT * FROM main.data_jobs;
+
+SELECT COUNT(*) FROM main.data_jobs;
+
+SELECT * FROM 	main.data_jobs WHERE salary_in_usd < 50000;
+
+SELECT
+	COUNT(*) AS number_rows_lower_than_50k
+FROM
+	main.data_jobs
+WHERE
+	salary_in_usd < 50000;
+
+--focus on experience_level
+SELECT
+	DISTINCT experience_level
+FROM
+	main.data_jobs;
+
+
+SELECT
+	*
+FROM
+	main.data_jobs
+WHERE
+	experience_level = 'EN';
+
+
+
+SELECT
+	MEDIAN(salary_in_usd)
+FROM
+	main.data_jobs
+WHERE
+	experience_level = 'EN';
+
+
+
+SELECT
+	MEDIAN(salary_in_usd)
+FROM
+	main.data_jobs
+WHERE
+	experience_level = 'MI';
+
+
+
+
+
+
+
+
+
+
